60c9a0b2669ec895898dc2a796ceec02ac4d529d
returnwellbeing <returnwellbeing@gmail.com>
2018-07-14 23:32:09 +0900

    심볼릭 링크 상대경로에서 추가 할 수 있게 수정. pkg 설치 부분 오타 수정.

--- set.sh
+++ set.sh
@@ -10,26 +10,26 @@ python2.7-dev \
 python3-dev \
 clang \
 gcc \
-clang-format
+clang-format \
 cmake \
 build-essential \
 ncurses-dev \
 texinfo \
 
 #sym link
-ln -s ./.gdbinit ~/
-ln -s ./.vimrc ~/
-ln -s ./.zshrc ~/
-ln -s ./.dircolors-zenburn ~/
-ln -s ./.dircolors-solarized ~/
-ln -s ./.vim ~/
-ln -s ./.oh-my-zsh ~/
-ln -s ./.tmux.conf ~/
-ln -s ./.tmux.conf.local ~/
+ln -rs ./.gdbinit ~/
+ln -rs ./.vimrc ~/
+ln -rs ./.zshrc ~/
+ln -rs ./.dircolors-zenburn ~/
+ln -rs ./.dircolors-solarized ~/
+ln -rs ./.vim ~/
+ln -rs ./.oh-my-zsh ~/
+ln -rs ./.tmux.conf ~/
+ln -rs ./.tmux.conf.local ~/
 
 #vim compile
 cd ./.vim_source
-.vim_source/configure --enable-pythoninterp \
+./configure --enable-pythoninterp \
 --with-python-config-dir=/usr/lib/python2.7/config
 make install
 cd ..
