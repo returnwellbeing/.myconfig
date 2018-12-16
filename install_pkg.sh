2621b1703f0198b02ac17f6a3aeb5dc32c9c7ea9
returnwellbeing <returnwellbeing@gmail.com>
2018-12-16 18:22:15 +0900

    vim configure에 필요한 pkg 추가

--- install_pkg.sh
+++ install_pkg.sh
@@ -34,7 +34,17 @@ pkg_array=( "tmux"
             "xdg-utils"
             "curl"
             "nodejs"
-            "dconf-editor")
+            "dconf-editor"
+            "libncurses5-dev"
+            "libgnome2-dev"
+            "libgnomeui-dev"
+            "libgtk2.0-dev"
+            "libatk1.0-dev"
+            "libbonoboui2-dev"
+            "libcairo2-dev"
+            "libx11-dev"
+            "libxpm-dev"
+            "libxt-dev")
 
 
 echo "=== PKG INSTALL START ==="
