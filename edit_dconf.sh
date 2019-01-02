917367b7e8e75d1dce4a6cca8fc6f99d131835cc
returnwellbeing <returnwellbeing@gmail.com>
2019-01-02 22:28:25 +0100

    CLI환경에서만 사용. 불필요한 설치 스크립트 삭제

--- edit_dconf.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-BASEDIR=$PWD
-
-### DCONF EDIT ###
-echo "=== DCONF EDIT START ==="
-dconf load /org/gnome/terminal/legacy/profiles:/ < ./.terminal.dconf
-echo "=== DCONF EDIT END ==="
